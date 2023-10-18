86459440f61afa4972b0c570d29269c11aec7fce
osama <osama@gmail.com>
2023-10-19 01:52:19 +0300

    commit

---
+++ lib/screens/tesst.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ossmkdm extends StatelessWidget {
+  const ossmkdm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
